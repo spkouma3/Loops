c6bd3854345ef9814977e8dacf71ae965fa0c223
spkouma3 <77463369+spkouma3@users.noreply.github.com>
2021-04-12 11:43:24 -0400

    Add files via upload

--- funcStore.py
+++ funcStore.py
@@ -7,130 +7,120 @@ plt.rcParams.update({'font.size': 18})
 
 # In[] Constants of Approximation
 
-res=[]
-
-reslog_Long=[]
-
 #constant μ, provided in Madras et al table 1.1
 # mu_2=2.638
-mu_2=3
-print("mu_2=",mu_2)
+mu2=3
 
-
-s_lp=10
-A_lp=10
+#A_lp=10
 #constant μ, provided in Madras et al table 1.1
 # mu_3=4.684
 mu3=5
-# print("mu_3=",mu_3)
 
 #Amplitude constant, provided by res paper
 #https://polymerdatabase.com/polymer%20physics/SAW.html
 # A_2=1.1771
 A2=1
 
-
-
 #Amplitude constant, provided by res paper
 #https://polymerdatabase.com/polymer%20physics/SAW.html
 # A_3=1.205
 A3=1
-# print("A_3=",A_3)
 
-# number of short-mers in a large-mer, chain length "r"
-# 6200/27=229, but here we want everything in terms of mers
-r_Long=6200 
-
-
-
-# number of single-mers in a arbitrary mer, chain length "r"
-r_Arb=100
-
-
-
-# number of single-mers in a arbitrary mer, chain length "r"
-r_Short=27
-#
-#
 nsurf=142600
 
 lamda=4
 
-#na is the number of chains so to get a value we must divide nsurf/r and then make it a multiple
-#for longs 27 is 4 more than the surface pancake max...
-n=27
-
 B3=1.1213
 
-
 # In[]
-def _na(_r):
+def na(_r):
     return int(nsurf/_r)
-
-def nvol(_na):
-    return 1
 
 def qvol(_r):
     return (A3*mu3)^_r
 
-def nlp(_r,_na):
-    return (_r-Ell(_na))/(s_lp-1)
+def nlp(_r,_na,_slp):
+    return (_r-(nsurf/_na))/(_slp-1)
 
-def nb(_na):
+def Ell(_r,_na,_slp):
+    return _r-nlp(_r,_na,_slp)*(_slp-1)
+
+def nb(_na,n):
     return n-_na
 
-def Ell(_na,_r,_slp):
-    return _r-nlp(_r,_na)*(_slp-1)
-
 def stirl(x):
-    if x<6:
-        prod=np.log(sp.factorial(x))
-    elif x>5:
-        prod=np.log(sp.factorial(5))+((x-10)*np.log(x-10)-(x-10))
-    return prod
+    return x*np.log(x)-(x)
 
 #yes lambda is spelled wrong, as lambda is a cmd in python
-def logwnaplusone(_na):
-    return stirl(nsurf-_na*Ell(_na))\
-    -stirl(nsurf-(_na+1)*Ell(_na))\
+def logw(_j,_na,_r,_slp):
+    return stirl(\
+        nsurf\
+        -\
+        _j\
+        *\
+        Ell(_r,_na,_slp)\
+        )\
+    -stirl(
+        nsurf-(_j+1)*Ell(_r,_na,_slp)
+        )\
     +np.log(nsurf)
     +np.log(lamda)\
     -2*np.log(lamda-1)\
-    +Ell(_na)*\
+    +Ell(_j)*\
     (\
     np.log(lamda-1)\
     -\
     np.log(nsurf)
     )
         
-def ScrSlp(_r,_na):
-    return stirl(Ell(_na))\
-    -stirl(nlp(_r,_na))\
-        
-
-def logRvol(_na):
-    return np.log(nb)\
+def ScrSlp(_na,_r,_slp):
+    return stirl(Ell(_r,_na,_slp))\
     -\
-    np.log(nvol(_na))\
+    stirl(nlp(_r,_na,_slp))\
+    -\
+    stirl(\
+        Ell(_r,_na,_slp) 
+        -\
+        nlp(_r,_na,_slp)\
+        )
+
+def logRvol(_na,_nvol,_n):
+    return np.log(nb(_na,_n))\
+    -\
+    np.log(_nvol)\
     -\
     np.log(qvol(_na))
         
-
-def logRF(_na):
-    return logwnaplusone(_na)-np.log(_na)
+def logRF(_r,_na,_slp):
+    s=0
+    splus=0
+    narange=np.arange(1,_na+1)
+    #if you print youll see arange only gives one less
+    naplusone=np.arange(1,_na+2)
+    for j in narange:
+        s+=logw(j,_na,_r,_slp)
+    for j in naplusone:
+        splus+=logw(j,_na,_r,_slp)
+    return splus\
+        -\
+        s\
+        -\
+        np.log(_na+1)
 
 def logRlp(_na,_r,_slp):
     return (_na+1)*\
-    ScrSlp(_na+1)\
-    -_na*ScrSlp(_na)\
+    ScrSlp(_na+1,_r,_slp)\
+    -_na*ScrSlp(_na,_r,_slp)\
     +(_r/(_slp-1))*\
     (\
-    np.log(mu_3)\
+    np.log(mu3)\
     +np.log(B3)\
     +3.7*np.log(_slp)\
     )
 
-def DeltaQlog(_na):
-    return logRF(_na)+logRlp(_na)+logRvol(_na)
-
-
+def DeltaQlog(_na,_r,_slp,_nvol,_n):
+    return logRF(_r,_na,_slp)
+    +\
+    logRlp(_na,_r,_slp)\
+    +\
+    logRvol(_na,_nvol,_n)
--- mainAPR2021.py
+++ mainAPR2021.py
@@ -4,63 +4,44 @@ import math as math
 import matplotlib.pyplot as plt 
 import scipy.special as sp
 plt.rcParams.update({'font.size': 18})
+from funcStore import *
 #
-# In[] Constants of Approximation
 
-res=[]
-
-reslog_Long=[]
-
-#constant μ, provided in Madras et al table 1.1
-# mu_2=2.638
-mu_2=3
-print("mu_2=",mu_2)
-
-
-s_lp=10
-A_lp=10
-#constant μ, provided in Madras et al table 1.1
-# mu_3=4.684
-mu_3=5
-print("mu_3=",mu_3)
-
-#Amplitude constant, provided by res paper
-#https://polymerdatabase.com/polymer%20physics/SAW.html
-# A_2=1.1771
-A_2=1
-
-
-
-#Amplitude constant, provided by res paper
-#https://polymerdatabase.com/polymer%20physics/SAW.html
-# A_3=1.205
-A_3=1
-print("A_3=",A_3)
+# In[] Some new things, kind of like a main.
 
 # number of short-mers in a large-mer, chain length "r"
 # 6200/27=229, but here we want everything in terms of mers
-r_Long=6200 
+rLong=6200 
 
 
 
 # number of single-mers in a arbitrary mer, chain length "r"
-r_Arb=100
+rArb=100
 
 
 
 # number of single-mers in a arbitrary mer, chain length "r"
-r_Short=27
-#
-#
-# In[] 
-# Grabbed 143750 from other project, "a"
-#this value is single mer sites (not short mer)
-#
-n_surf=142600
-print(n_surf)
-# In[] Some new things, kind of like a main.
+rShort=27
+
+slp=2
+#na is the number of chains so to get a value we must divide nsurf/r and then make it a multiple
+#for longs 27 is 4 more than the surface pancake max...
+# n=27
+
+naNew=na(_r=rLong)+2
+
+#total chains
+n=30
+
+nvol=nsurf
 
 
+dQArr=[]
+
+
+
+for k in range(naNew,naNew+4):
+    dQArr.append(DeltaQlog(k,_r=rLong,_slp=slp,_nvol=nvol,_n=n))
 
 # In[]
 # n_0=n_a(r_Long)+2
@@ -74,18 +55,18 @@ print(n_surf)
 # In[] Multi plot option
 #
 #
-plt.figure()   
-x_Long = np.array(r)
-y_Long = reslog_Long
-x_Line = r
-y_Line = np.zeros(r.size)
-# plt.xticks(range(min(x_Long), max(x_Long)+1))
-# # Multiple plots
-plt.xlabel('${n_a}$')
-plt.ylabel('$Log(f(n_a))$')
-plt.grid(b=None, which='major', axis='both')
-# plt.title('Looping Behaviour Long') 
-plt.plot(x_Long, y_Long,linewidth=2) 
-#threshhold
-plt.plot(x_Line, y_Line,'--r',linewidth=2)
+# plt.figure()   
+# x_Long = np.array(r)
+# y_Long = reslog_Long
+# x_Line = r
+# y_Line = np.zeros(r.size)
+# # plt.xticks(range(min(x_Long), max(x_Long)+1))
+# # # Multiple plots
+# plt.xlabel('${n_a}$')
+# plt.ylabel('$Log(f(n_a))$')
+# plt.grid(b=None, which='major', axis='both')
+# # plt.title('Looping Behaviour Long') 
+# plt.plot(x_Long, y_Long,linewidth=2) 
+# #threshhold
+# plt.plot(x_Line, y_Line,'--r',linewidth=2)
 #
